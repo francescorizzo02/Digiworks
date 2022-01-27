70fa2d8da25719cbc37771342f69ec11e7af30a5
francescorizzo02 <85279924+francescorizzo02@users.noreply.github.com>
2022-01-27 12:34:20 +0100

    Added the application loader (#3)

--- server.ts
+++ server.ts
@@ -1,5 +1,9 @@
-class Server {
+import Application from "./loaders/application.loader";
+
+export default class Server extends Application {
     constructor() {
-        
+        super();
     }
 }
+
+new Server();
