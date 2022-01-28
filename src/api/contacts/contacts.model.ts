7c7a451c4b5df074baa109313025b24e9627647b
francescorizzo02 <85279924+francescorizzo02@users.noreply.github.com>
2022-01-28 16:12:48 +0100

    Added contacts model (#11)

---
+++ src/api/contacts/contacts.model.ts
@@ -0,0 +1,25 @@
+//import dependences
+import mongoose from "mongoose";
+
+//import loaders
+import { connect } from "../../../loaders/database.loaders";
+
+//import schemas
+import ContactSchema from "./contacts.schema";
+
+class ContactModel {
+  private _contactModel: mongoose.Model<ContactModel, {}, {}, {} | undefined>;
+  
+  constructor() {
+    this._setModel();
+    this._contactModel = this.getModel();
+  }
+
+  public getModel(): mongoose.Model<ContactModel, {}, {}, {} | undefined> {
+    return this._contactModel;
+  }
+
+  private _setModel() {
+    this._contactModel = connect().model("Contact", ContactSchema)
+  }
+}
---
+++ src/api/contacts/contacts.schema.ts
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+let ContactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  surname: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  fullname: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  dataDiNascita: {
+    type: Date,
+  },
+  luogoDiNascita: {
+    type: String,
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  indirizzo: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default ContactSchema;
+
+export interface ContactInterface {
+  name: string;
+  surname: string;
+  fullname: string;
+  email: string;
+  dataDiNascita: Date;
+  luogoDiNascita: string;
+  phoneNumber: string;
+  indirizzo: string;
+  createdAt: Date;
+  isDisabled: boolean;
+  _id: mongoose.Schema.Types.ObjectId;
+}
+
+let ContactTree = ContactSchema.obj;
+
+ContactTree._id = {};
+
+export { ContactTree };
